5b76c5b41634edeaeeb1ed526657305ec5782f2d
Nu4 <mauriciocruzisc@gmail.com>
2022-01-17 15:53:26 -0600

    Prowater

--- ProWater/ViewController.swift
+++ ProWater/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtuser: UITextField!
+    @IBOutlet weak var txtpassword: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        txtuser.placeholder = "Username"
+        txtpassword.placeholder = "Password"
+        let userImage = UIImage(named: "person")
+        addleftImage(txtFiedld: txtuser, andImage: userImage!)
+        
+        let passImage = UIImage(named: "lock")
+        addleftImage(txtFiedld: txtpassword, andImage: passImage!)
+        
     }
-
+    
+    func addleftImage(txtFiedld:UITextField,andImage img:UIImage){
+        let leftImageView = UIImageView(frame: CGRect(x:0.0,y:0.0,width:img.size.width,height: img.size.height))
+        leftImageView.image = img
+        txtFiedld.leftView  =   leftImageView
+        txtFiedld.leftViewMode = .always
+    }
 
 }
 
